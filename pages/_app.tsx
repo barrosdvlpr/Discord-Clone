43233f3a2aa8fbb7cc8121e5e70fd074ec4cee8b
barrosdvlpr <barrosdvlpr@gmail.com>
2022-11-25 09:29:18 -0300

    add transition effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,21 +11,43 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
   <>
-  <div className="flex h-screen ">
-    <div className="bg-gray-900 text-gray-100 min-h-screen p-3 space-y-2 overflow-y-scroll min-w-min">
-      <Link legacyBehavior href="/">
-        <a className={`
-        ${router.pathname === "/" ? "rounded-2xl bg-brand text-white" : "hover:rounded-2xl hover:bg-brand hover:text-white text-gray-100 bg-gray-700"}
-          w-12 h-12 rounded-3xl  flex items-center justify-center transition-all duration-200`}
-        ><DiscordIcon className="w-7 h-5 "/></a>
-      </Link>
-      <Link legacyBehavior href="/servers/1">
-        <a className={`bg-gray-700 text-gray-100 w-12 h-12 rounded-3xl hover:rounded-xl flex items-center justify-center hover:bg-brand hover:text-white transition-all duration-200`}
-        >S1</a>
-      </Link>
+    <div className="flex h-screen text-gray-100">
+      <div className="p-3 space-y-2 overflow-y-scroll bg-gray-900">
+        <Link legacyBehavior href="/">
+          <a className="relative block group">
+            <div className="absolute flex items-center h-full -left-3">
+              <div
+                className={`${
+                  router.pathname === "/"
+                    ? "h-10"
+                    : "h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100"
+                } w-1 transition-all duration-200 origin-left bg-white rounded-r`}
+              ></div>
+            </div>
+
+            <div className="group-active:translate-y-px">
+              <div
+                className={`${
+                  router.pathname === "/"
+                    ? "rounded-2xl bg-brand text-white"
+                    : "text-gray-100 group-hover:rounded-2xl group-hover:bg-brand group-hover:text-white bg-gray-700 rounded-3xl"
+                } flex items-center justify-center w-12 h-12 transition-all duration-200`}
+              >
+                <DiscordIcon className="h-5 w-7" />
+              </div>
+            </div>
+          </a>
+        </Link>
+
+        <Link legacyBehavior href="/servers/1">
+          <a className="flex items-center justify-center w-12 h-12 text-gray-100 transition-all duration-200 bg-gray-700 rounded-3xl hover:rounded-2xl hover:bg-brand hover:text-white">
+            S1
+          </a>
+        </Link>
+      </div>
+
+      <Component {...pageProps} />
     </div>
-    <Component {...pageProps}/>
-  </div>
   </>  
   )
 }
