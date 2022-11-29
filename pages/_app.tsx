aa38c26218b21a3b3bbf4fd351d6f523bf75a139
barrosdvlpr <barrosdvlpr@gmail.com>
2022-11-29 08:02:43 -0300

    styling new dynamic url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,7 +30,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           <hr className="border-t-white/[.06] border-t-2 rounded mx-2" />
 
           {servers.map((server) => (
-            <NavLink href={`/servers/${server.id}`} key={server.id}>
+            <NavLink href={`/servers/${server.id}/channels/1`}
+              active={+router.query.sid === +server.id}
+              key={server.id}>
               <img src={`/servers/${server.img}`} alt="" />
             </NavLink>
           ))}
@@ -56,8 +58,9 @@ function Discord(props: any) {
   );
 }
 
-export function NavLink({href, children}: any){
+export function NavLink({href, active, children}: any){
   const router = useRouter()
+  active ||= router.asPath === href
 
   return(
     <Link legacyBehavior href={href}>
@@ -65,7 +68,7 @@ export function NavLink({href, children}: any){
         <div className="absolute flex items-center h-full -left-3">
           <div
             className={`${
-              router.asPath === href
+              active
                 ? "h-10"
                 : "h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100"
             } w-1 transition-all duration-200 origin-left bg-white rounded-r`}
@@ -75,7 +78,7 @@ export function NavLink({href, children}: any){
         <div className="group-active:translate-y-px">
           <div
             className={`${
-              router.asPath === href
+              active
                 ? "rounded-2xl bg-brand text-white"
                 : "text-gray-100 group-hover:rounded-2xl group-hover:bg-brand group-hover:text-white bg-gray-700 rounded-3xl"
             } flex items-center justify-center w-12 h-12 transition-all duration-200 overflow-hidden`}
--- pages/servers/[sid]/channels/[cid].js
+++ pages/servers/[sid]/channels/[cid].js
@@ -1,9 +1,9 @@
-// import { CheckIcon, ChevronIcon, VerifiedIcon, BookIcon, AddPersonIcon, SpeakerphoneIcon, ArrowIcon } from "../../components/icons";
-import * as Icons from "../../components/icons";
-import data from "../../data.json";
+import * as Icons from "../../../../components/icons";
+import data from "../../../../data.json";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Server() {
-
   return (
     <>
       <div className="flex flex-col bg-gray-800 w-60">
@@ -55,17 +55,22 @@ export default function Server() {
 
 function ChannelLink({ channel }) {
   const Icon = channel.icon ? Icons[channel.icon] : Icons.Hashtag;
+  const router = useRouter();
+  const active = +channel.id === +router.query.cid;
 
   return (
-    <a
-      key={channel.id}
-      href="#"
-      className="flex items-center px-2 mx-2 py-1 text-gray-300 hover:text-gray-100 hover:bg-gray-550/[0.16] rounded group"
-    >
-      <Icon className="w-5 h-5 mr-1.5 text-gray-400" />
-      {channel.label}
-      <Icons.AddPerson className="w-4 h-4 ml-auto text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
-    </a>
+    <Link legacyBehavior href={`/servers/1/channels/${channel.id}`}>
+      <a
+        className={`${
+          active
+            ? "text-white bg-gray-550/[0.32]"
+            : "text-gray-300 hover:text-gray-100 hover:bg-gray-550/[0.16]"
+        } flex items-center px-2 mx-2 py-1 rounded group`}
+      >
+        <Icon className="w-5 h-5 mr-1.5 text-gray-400" />
+        {channel.label}
+        <Icons.AddPerson className="w-4 h-4 ml-auto text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
+      </a>
+    </Link>
   );
 }
-  
